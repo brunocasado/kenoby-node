2b967ad4d083f36e7742af19c4f93962f6cb0825
Bruno Casado <bagcnop@gmail.com>
2017-05-19 18:25:36 -0300

    fixing things testing...

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const __VERSION = '0.0.1';
 
 var opts = opts || {
   _request: {
-    url: 'api.kenoby.com',
+    url: 'https://api.kenoby.com',
     headers: {}
   }
 };
@@ -20,41 +20,43 @@ var _setAccess = function (username, password) {
 
 var _setHeaders = function () {
 
-  if (opts.tenant) throw new Error('We need tenant for access kenoby api');
-  if (opts.accessToken) throw new Error('We need your accessToken for access kenoby api');
+  if (!opts.tenant) throw new Error('We need tenant for access kenoby api');
+  if (!opts.accessToken) throw new Error('We need your accessToken for access kenoby api');
 
   opts._request.headers = {
     'x-tenant': opts.tenant,
     'x-version': '3.0.0',
     'content-type': 'json',
-    'Authorization': opts.accessToken
+    'Authorization': 'Basic ' + opts.accessToken
   }
+  console.log(opts);
 }
 
 // @todo implement all methods ref.: api.kenoby.com
 // @todo implement a crud interface for request
 
-var _requestCallback = function (error, response, body) {
-  if(!error && response.statusCode == 200) {
-    try {
-      const data = JSON.parse(body);
-      resolve(data);
-    } catch (error) {
-      reject(error.getMessage());
-    }
-  } else {
-    reject(error);
-  }
-}
 
 var _getPositions = function () {
+  _setHeaders(); // temporary
   return new Promise(function (resolve, reject) {
-    request.get(opts._request.url + '/positions', _requestCallback);
+    request.get(opts._request.url + '/positions', function (error, response, body) {
+      console.log(body);
+      if (!error && response.statusCode == 200) {
+        try {
+          const data = JSON.parse(body);
+          resolve(data);
+        } catch (error) {
+          reject(error.getMessage());
+        }
+      } else {
+        reject(error);
+      }
+    });
   });
 }
 
 module.exports = {
-  setTenant:_setTenant,
+  setTenant: _setTenant,
   setAccess: _setAccess,
   getPositions: _getPositions
 };
--- tests/kenoby/index.js
+++ tests/kenoby/index.js
@@ -1,16 +1,12 @@
 var test = require('tape');
 var kenoby = require('../../index.js');
 
-test('describe instance', function(t) {
+test('describe instance', function (t) {
 
   kenoby.setTenant('58c821a0a1f3f6001a3bdd26');
   kenoby.setAccess('caiom@drconsulta.com', 'drcKenoby123');
-  kenoby.getPositions().then(function(data){
+  kenoby.getPositions().then(function (data) {
     console.log(data);
 
-  });
-    
-
-
-
+  }).catch(console.log);
 });
