992b300b07f7fe0ed384d8fa52fe963f31214119
Bruno Casado <bagcnop@gmail.com>
2017-05-22 11:28:10 -0300

    test if return is array

--- tests/kenoby/index.js
+++ tests/kenoby/index.js
@@ -6,7 +6,7 @@ test('describe instance', function (t) {
   kenoby.setTenant('58c821a0a1f3f6001a3bdd26');
   kenoby.setAccess('caiom@drconsulta.com', 'drcKenoby123');
   kenoby.getPositions().then(function (data) {
-    console.log(data);
-
+    t.equal(data.constructor, Array );
+    t.end();
   }).catch(console.log);
 });
